13d9d14ef81a393fd98f0d61fc0e772938b05cd6
RojanShrestha1992 <136217012+RojanShrestha1992@users.noreply.github.com>
2026-01-27 17:58:00 +0545

    render html file

---
+++ render_html.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from 'path'
+const app = express()
+    const absPath = path.resolve('view')
+
+app.get("/", (req,resp)=>{
+        resp.sendFile(absPath+"/home.html")
+
+})
+app.get("/login", (req,resp)=>{
+    // console.log(absPath)
+        resp.sendFile(absPath+"/login.html")
+
+
+})
+app.get("/about", (req,resp)=>{
+        resp.sendFile(absPath+"/about.html")
+
+})
+app.use((req,resp)=>{
+
+    resp.status(404).sendFile(absPath+"/404.html")
+
+})
+app.listen(3200)
